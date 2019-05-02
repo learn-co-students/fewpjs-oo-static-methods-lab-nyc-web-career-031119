a0a6eee174a055bb2cf4b30ae399188734c5bdb6
ezevin <ehzevin@gmail.com>
2019-05-02 10:35:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9 '-]/g, '')
+  }
+
+  static titleize(string){
+    let words = string.split(" ");
+    let sentance = [];
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    for (let i=0; i<words.length; i++){
+    if (i ==0){
+      sentance.push(this.capitalize(words[i]))
+    }
+    else {
+      if (exceptions.includes(words[i])){
+        sentance.push(words[i])
+      }
+      else {
+        sentance.push(this.capitalize(words[i]))
+      }
+      }
+    }
+    return sentance.join(" ")
+  }
+
+
 }
