22b574241086f5ad8b2ff00a8d788c505b354719
whosAle <abelgrave95@gmail.com>
2019-05-05 10:34:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.substr(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  // static upperCaseChar(match, offset, string) {
+  //   return match.toUpperCase();
+  // }
+
+  static titleize(string) {
+    // return string.replace(/(?<!^)\b(?:the|a|an|but|of|and|for|at|by|from)\b(*SKIP)(*FAIL)|\b(\pL)/g, this.upperCaseChar);
+    let exceptions =["the","a","an","but","of","and","for","at","by","from"];
+    return string.split(" ").map((word, index) => (exceptions.includes(word) && (index!=0))? word : this.capitalize(word)).join(" ");
+  }
 }
