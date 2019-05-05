6b584338eb76500dcbbdcf9778da0b6759e6e62a
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-05-05 13:39:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(str) {
+    let exempt =["the","a","an","but","of","and","for","at","by","from"];
+ return str.split(" ").map((word, index) => (exempt.includes(word) && (index!=0))? word : this.capitalize(word)).join(" ");
+  }
 }
